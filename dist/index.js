412ea036bc4c802846d3f44cc030d4dcc9d800a6
asad755 <143116178+asad755@users.noreply.github.com>
2024-01-06 01:49:48 +0500

    first Allah commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function addTwoNumbers(number1, number2) {
+    const result = number1 + number2;
+    console.log(result);
+}
+addTwoNumbers(2, 2);
+addTwoNumbers(4, 2);
+addTwoNumbers(2, 3);
+addTwoNumbers(2, 9);
+/*
+console.log('marhaba world',10.09);
+*/
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function addTwoNumbers(number1: number, number2: number){
+    const result = number1 + number2;
+    console.log(result);
+}
+
+addTwoNumbers(2,2);
+addTwoNumbers(4, 2);
+addTwoNumbers(2, 3);
+addTwoNumbers(2, 9);
+
+
+
+/*
+console.log('marhaba world',10.09);
+*/
